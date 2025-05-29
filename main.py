b9aeeaf7059d719340291328680b3cce30222f5d
mkonchev <maksim1901m@gmail.com>
2025-05-29 15:50:02 +0300

    rename main.py & create database.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
+from typing import Literal
 
 
 class CatalogItem(BaseModel):
     name: str
-    category: tuple[1, 2, 3, 4, 5]
+    category: Literal[1, 2, 3, 4, 5]
     price: int
 
 
-app = FastAPI
+app = FastAPI()
 
 
 @app.post("/create_items/{item_id}")
@@ -25,4 +26,4 @@ def homepage():
 @app.get("/get_items/{item_id}")
 def read_item(item_id: int):
     print("item_id", item_id)
-    return {"item_id", item_id}
+    return {"item_id": item_id}
