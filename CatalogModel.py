c80d85c5455233c011060f6bb226a26c7c9f6124
mkonchev <maksim1901m@gmail.com>
2025-05-29 14:16:52 +0300

    create catalog item model & FastAPI methods

---
+++ CatalogModel.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+
+class CatalogItem(BaseModel):
+    name: str
+    category: tuple[1, 2, 3, 4, 5]
+    price: int
+
+
+app = FastAPI
+
+
+@app.post("/create_items/{item_id}")
+def create_item(item: CatalogItem, item_id: int):
+    return JSONResponse({"item": item.model_dump(), "item_id": item_id})
+
+
+@app.get("/")
+def homepage():
+    return JSONResponse({'ты че тут забыл': '?'})
+
+
+@app.get("/get_items/{item_id}")
+def read_item(item_id: int):
+    print("item_id", item_id)
+    return {"item_id", item_id}
