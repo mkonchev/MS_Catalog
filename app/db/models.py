00f28e213ec688ed59c15bf8c770a2c40eb0a2e8
mkonchev <maksim1901m@gmail.com>
2025-06-23 00:06:00 +0300

    fix db connection

---
+++ app/db/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Enum
+from app.schemas.models.Category import Category as CategoryEnum
+from db.database import engine
+
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = "catalog_items"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String)
+    category = Column(Enum(CategoryEnum))
+    price = Column(Integer)
+
+
+Base.metadata.create_all(bind=engine)
